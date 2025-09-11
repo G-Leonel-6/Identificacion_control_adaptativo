873a5380fb26168e79e7690313f222ade9d7a610
G-Leonel-6 <legonzalez@fi.uba.ar>
2025-09-10 23:33:45 -0300

    correcion en los codigos de simulacion embebida

--- simulador/src/main.cpp
+++ simulador/src/main.cpp
@@ -6,12 +6,12 @@ double state[4] = {0.0, 0.0, 0.1, 0.0};
 
 // Matriz A_d y B_d calculadas offline en Python y copiadas acá
 const double Ad[4][4] = {
-    {1.0, 0.0995, 0.0048, 0.00016},
-    {0.0, 0.99, 0.094, 0.0048},
-    {0.0, 0.00098, 0.89, 0.09},
-    {0.0, 0.0191, -2.0, 0.894}
+    {1.0, 0.0995024709, 0.00480129762, 0.000161339966},
+    {0.0, 0.990082021, 0.0941298848, 0.00480129762},
+    {0.0, 0.000978857822, 0.894048773, 0.0964424206},
+    {0.0, 0.0191905983, -2.08046006, 0.894048773}
 };
-const double Bd[4] = {0.00497, 0.099, -0.0097, -0.191};
+const double Bd[4] = {0.00497529, 0.09917979, -0.00978858, -0.19190598};
 
 void setup() {
   Serial.begin(115200);
@@ -28,18 +28,20 @@ void loop() {
   // Copiar el nuevo estado
   for (int i = 0; i < 4; i++) state[i] = new_state[i];
 
-  // Mostrar por serial
-  Serial.print("x:");
-  Serial.println(state[0]);
-
-  Serial.print("dx:");
-  Serial.println(state[1]);
-
-  Serial.print("theta:");
-  Serial.println(state[2]);
-
-  Serial.print("dtheta:p");
   Serial.println(state[3]);
 
-  delay(10);  // 10 ms ≈ Ts
+  // Mostrar por serial
+  Serial.print(">x:");
+  Serial.println(state[0],4);
+
+  Serial.print(">dx:");
+  Serial.println(state[1],4);
+
+  Serial.print(">theta:");
+  Serial.println(state[2],4);
+
+  Serial.print(">dtheta:");
+  Serial.println(state[3],4);
+
+  delay(100);  // 10 ms ≈ Ts
 }
--- simulador/src/pendulum.cpp
+++ simulador/src/pendulum.cpp
@@ -1,5 +1,6 @@
 // pendulum.cpp
 #include "pendulum.h"
+#include <Arduino.h>
 
 void step_discrete(const double Ad[4][4], const double Bd[4],
                    const double x[4], double u, double x_next[4]) {
@@ -8,6 +9,6 @@ void step_discrete(const double Ad[4][4], const double Bd[4],
         for (int j = 0; j < 4; j++) {
             sum += Ad[i][j] * x[j];
         }
-        x_next[i] = sum + Bd[i] * u;
-    }
+        x_next[i] = sum + Bd[i] * u;     
+        }
 }
